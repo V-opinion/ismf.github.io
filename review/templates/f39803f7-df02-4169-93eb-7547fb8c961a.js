a7f23e56fdfa33900f14c3aa7992f703c1c87db1
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2025-05-06 17:40:18 +0200

    Update f39803f7-df02-4169-93eb-7547fb8c961a.js

--- review/templates/f39803f7-df02-4169-93eb-7547fb8c961a.js
+++ review/templates/f39803f7-df02-4169-93eb-7547fb8c961a.js
@@ -2,7 +2,7 @@ var content='<div class="ui-page " deviceName="iphone15promax" deviceType="mobil
     <div id="t-f39803f7-df02-4169-93eb-7547fb8c961a" class="template growth-both devMobile devIOS iphone-device canvas firer commentable non-processed" alignment="left" name="Template 1"width="430" height="932">\
     <div id="backgroundBox"><div class="colorLayer"></div><div class="imageLayer"></div></div>\
     <div id="alignmentBox">\
-      <link type="text/css" rel="stylesheet" href="./review/templates/f39803f7-df02-4169-93eb-7547fb8c961a/style-1737766329957.css" />\
+      <link type="text/css" rel="stylesheet" href="https://afmc.multireligionvalsystem.eu.org/review/templates/f39803f7-df02-4169-93eb-7547fb8c961a/style-1737766329957.css" />\
       <div class="freeLayout">\
       </div>\
 \
